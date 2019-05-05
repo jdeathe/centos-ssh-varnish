d77fde36009a720708e7896d54d657b4c8f56281
James Deathe <james.deathe@gmail.com>
2019-05-05 21:48:42 +0100

    #171: Updates source image to 1.10.1.

--- src/opt/scmi/environment.sh
+++ src/opt/scmi/environment.sh
@@ -1,6 +1,6 @@
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Constants
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 readonly DOCKER_USER=jdeathe
 readonly DOCKER_IMAGE_NAME=centos-ssh-varnish
 
@@ -8,15 +8,14 @@ readonly DOCKER_IMAGE_NAME=centos-ssh-varnish
 readonly DOCKER_IMAGE_TAG_PATTERN='^(latest|centos-6|((1|centos-6-1)\.[0-9]+\.[0-9]+))$'
 readonly DOCKER_IMAGE_RELEASE_TAG_PATTERN='^(1|centos-6-1)\.[0-9]+\.[0-9]+$'
 
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Variables
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # Docker image/container settings
 DOCKER_CONTAINER_OPTS="${DOCKER_CONTAINER_OPTS:-}"
 DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG:-latest}"
-DOCKER_NAME="${DOCKER_NAME:-varnish.pool-1.1.1}"
-DOCKER_PORT_MAP_TCP_22="${DOCKER_PORT_MAP_TCP_22:-NULL}"
+DOCKER_NAME="${DOCKER_NAME:-varnish.1}"
 DOCKER_PORT_MAP_TCP_80="${DOCKER_PORT_MAP_TCP_80:-8000}"
 DOCKER_PORT_MAP_TCP_8443="${DOCKER_PORT_MAP_TCP_8443:-8500}"
 DOCKER_RESTART_POLICY="${DOCKER_RESTART_POLICY:-always}"
@@ -28,7 +27,7 @@ NO_CACHE="${NO_CACHE:-false}"
 DIST_PATH="${DIST_PATH:-./dist}"
 
 # Number of seconds expected to complete container startup including bootstrap.
-STARTUP_TIME="${STARTUP_TIME:-2}"
+STARTUP_TIME="${STARTUP_TIME:-3}"
 
 # Docker --sysctl settings
 SYSCTL_NET_CORE_SOMAXCONN="${SYSCTL_NET_CORE_SOMAXCONN:-1024}"
@@ -45,29 +44,17 @@ REGISTER_ETCD_PARAMETERS="${REGISTER_ETCD_PARAMETERS:-}"
 REGISTER_TTL="${REGISTER_TTL:-60}"
 REGISTER_UPDATE_INTERVAL="${REGISTER_UPDATE_INTERVAL:-55}"
 
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Application container configuration
-# -----------------------------------------------------------------------------
-SSH_AUTHORIZED_KEYS="${SSH_AUTHORIZED_KEYS:-}"
-SSH_AUTOSTART_SSHD="${SSH_AUTOSTART_SSHD:-true}"
-SSH_AUTOSTART_SSHD_BOOTSTRAP="${SSH_AUTOSTART_SSHD_BOOTSTRAP:-true}"
-SSH_CHROOT_DIRECTORY="${SSH_CHROOT_DIRECTORY:-%h}"
-SSH_INHERIT_ENVIRONMENT="${SSH_INHERIT_ENVIRONMENT:-false}"
-SSH_SUDO="${SSH_SUDO:-ALL=(ALL) ALL}"
-SSH_USER="${SSH_USER:-app-admin}"
-SSH_USER_FORCE_SFTP="${SSH_USER_FORCE_SFTP:-false}"
-SSH_USER_HOME="${SSH_USER_HOME:-/home/%u}"
-SSH_USER_ID="${SSH_USER_ID:-500:500}"
-SSH_USER_PASSWORD="${SSH_USER_PASSWORD:-}"
-SSH_USER_PASSWORD_HASHED="${SSH_USER_PASSWORD_HASHED:-false}"
-SSH_USER_SHELL="${SSH_USER_SHELL:-/bin/bash}"
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 VARNISH_AUTOSTART_VARNISHD_WRAPPER="${VARNISH_AUTOSTART_VARNISHD_WRAPPER:-true}"
 VARNISH_AUTOSTART_VARNISHNCSA_WRAPPER="${VARNISH_AUTOSTART_VARNISHNCSA_WRAPPER:-false}"
 VARNISH_MAX_THREADS="${VARNISH_MAX_THREADS:-1000}"
 VARNISH_MIN_THREADS="${VARNISH_MIN_THREADS:-50}"
+VARNISH_OPTIONS=""
 VARNISH_STORAGE="${VARNISH_STORAGE:-file,/var/lib/varnish/varnish_storage.bin,1G}"
 VARNISH_THREAD_TIMEOUT="${VARNISH_THREAD_TIMEOUT:-120}"
 VARNISH_TTL="${VARNISH_TTL:-120}"
 VARNISH_VARNISHNCSA_FORMAT="${VARNISH_VARNISHNCSA_FORMAT:-"%h %l %u %t \"%r\" %s %b \"%{Referer}i\" \"%{User-agent}i\""}"
+VARNISH_VARNISHNCSA_OPTIONS=""
 VARNISH_VCL_CONF="${VARNISH_VCL_CONF:-/etc/varnish/docker-default.vcl}"
